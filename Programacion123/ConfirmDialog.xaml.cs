12946f5581ab49c046e549e3b25df86b24b24d02
José Manuel (Chema) Solís <jsolis@alumnoifp.es>
2025-09-22 14:15:07 +0200

    Fixed same separator in two options confirm dialog and one option

--- Programacion123/ConfirmDialog.xaml.cs
+++ Programacion123/ConfirmDialog.xaml.cs
@@ -57,6 +57,9 @@ namespace Programacion123
             LabelAcceptSingle.Visibility = (_chooseType == ConfirmChooseType.acceptOnly ? Visibility.Visible : Visibility.Hidden);
             ButtonAcceptSingle.Visibility = (_chooseType == ConfirmChooseType.acceptOnly ? Visibility.Visible : Visibility.Hidden);
 
+            Separator1Option.Visibility = (_chooseType == ConfirmChooseType.acceptOnly ? Visibility.Visible : Visibility.Hidden);
+            Separator2Options.Visibility = (_chooseType != ConfirmChooseType.acceptOnly ? Visibility.Visible : Visibility.Hidden);
+
         }
 
         private void ConfirmDialog_Closed(object? sender, EventArgs e)
