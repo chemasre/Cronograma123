62c5b5faa6cc9e968ac343976cd0e5e481fa1803
José Manuel (Chema) Solís <jsolis@alumnoifp.es>
2025-10-12 11:26:21 +0200

    Updated version

--- Programacion123/_Source/Base/Constants.cs
+++ Programacion123/_Source/Base/Constants.cs
@@ -5,7 +5,7 @@
         public const string appName = "Programabara";
         public const string contactName = "Chema";
         public const string contactEmail = "chema.sre@gmail.com";
-        public const string version = "0.9.1";
+        public const string version = "0.9.2";
         public const string configFileName = "Config.json";
         public const float restartWaitTime = 2.0f;
         public const float informativeTaskWaitTime = 2.0f;
